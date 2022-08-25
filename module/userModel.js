786533e090996cf0be405b07fea64f46a1638f7b
suhotang <dkdudkc22@gmail.com>
2022-08-25 23:04:39 +0900

    Fix - timeout 설정, database 조회 쿼리 수정

--- module/userModel.js
+++ module/userModel.js
@@ -3,6 +3,10 @@ const mysql = require("mysql2/promise");
 module.exports = {
   getUserPaylod: async (snsId) => {
     try {
+      if (snsId) {
+        throw "유효하지 않은 snsId";
+      }
+
       // Mysql Connection
       const mysqlConnection = await mysql.createConnection({
         host: process.env.DB_HOST,
@@ -15,9 +19,9 @@ module.exports = {
       let isNew = false;
       let userId = null;
       const [users, fields] = await mysqlConnection.query(
-        `select * from user where sns_id=${snsId}`
+        `select * from user where sns_id='${snsId}'`
       );
-      console.log(users);
+      //console.log(users);
 
       if (Array.isArray(users) && users.length > 0) {
         console.log("이미 존재하는 사용자");
