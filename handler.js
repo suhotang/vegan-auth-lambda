132a1e1935a9eb5f7d1f2d940122f26ae3e61c00
suhotang <dkdudkc22@gmail.com>
2022-12-12 14:28:27 +0900

    Fix   - 관리자용 jwt secret key와 사용자용 secret key 분리   - 관리자 jwt payload에서 isAdmin 값 삭제, authority 값 추가

--- handler.js
+++ handler.js
@@ -13,7 +13,7 @@ api.post("/auth/token", async (req, res) => {
   // 401 Error Block
   try {
     // 1. body로 토큰을 받는다.
-    const body = req?.body
+    const body = req?.body;
     const provider = body?.provider;
     const token = body?.token;
 
@@ -25,7 +25,7 @@ api.post("/auth/token", async (req, res) => {
   } catch (e) {
     console.log("Error 401:", e);
     return res.status(401).json({
-      message: "사용자 검증 실패"
+      message: "사용자 검증 실패",
     });
   }
 
@@ -33,18 +33,32 @@ api.post("/auth/token", async (req, res) => {
   try {
     // 3. snsId로 user 테이블에서 유저가 이미 존재하는지 확인
     const { isNew, ...payload } = await getUserPaylod(snsId);
-    const { accessToken, refreshToken } = generateJwt(payload);
+    const { accessToken, refreshToken } = generateJwt(
+      payload,
+      process.env.JWT_SECRET_KEY
+    );
 
-    res.cookie("accessToken", accessToken, { maxAge: 3600 * 1000, httpOnly: true }).send();
-    res.cookie("refreshToken", refreshToken, { maxAge: 3600 * 1000, httpOnly: true }).send();
+    res
+      .cookie("accessToken", accessToken, {
+        maxAge: 3600 * 1000,
+        httpOnly: true,
+      })
+      .send();
+    res
+      .cookie("refreshToken", refreshToken, {
+        maxAge: 3600 * 1000,
+        httpOnly: true,
+      })
+      .send();
 
     return res.status(200).json({
-      message: "로그인 성공", data: { isNew }
+      message: "로그인 성공",
+      data: { isNew },
     });
   } catch (e) {
     console.log("Error 500:", e);
     return res.status(500).json({
-      message: "서버 내부 에러 발생"
+      message: "서버 내부 에러 발생",
     });
   }
 });
@@ -53,8 +67,7 @@ api.post("/auth/token", async (req, res) => {
 api.post("/auth/login", async (req, res) => {
   console.log("POST /auth/login");
   try {
-    // 1. body로 토큰을 받는다.
-    const body = req?.body
+    const body = req?.body;
     const id = body?.id;
     const password = body?.password;
 
@@ -63,45 +76,56 @@ api.post("/auth/login", async (req, res) => {
     // DB에서 id로 Row 조회
     // id가 db에 없으면 400 return (로그인 실패)
     const userRow = await getAdminUserById(id);
-    
+
     // row의 password와 받은 password 비교
     // password가 일치하지 않으면 400 return (로그인 실패)
     if (userRow) {
-      // let salt = CryptoJS.lib.WordArray.random(128 / 8);
-      // salt = CryptoJS.enc.Hex.stringify(salt);
       let cryptoInputPassword = CryptoJS.PBKDF2(password, userRow?.salt, {
         keySize: 256 / 32,
-        iterations: 1000
+        iterations: 1000,
       });
       cryptoInputPassword = CryptoJS.enc.Hex.stringify(cryptoInputPassword);
 
       if (userRow?.password !== cryptoInputPassword) {
         throw {
           statusCode: 400,
-          message: "올바르지 않은 password"
-        }
+          message: "올바르지 않은 password",
+        };
       }
     }
 
-    const payload = { userId: userRow?.userId, isAdmin: true };
-    const { accessToken, refreshToken } = generateJwt(payload);
+    const payload = { userId: userRow?.userId, authority: [] };
+    const { accessToken, refreshToken } = generateJwt(
+      payload,
+      process.env.ADMIN_JWT_SECRET_KEY
+    );
 
-    res.cookie("accessToken", accessToken, { maxAge: 3600 * 1000, httpOnly: true }).send();
-    res.cookie("refreshToken", refreshToken, { maxAge: 3600 * 1000, httpOnly: true }).send();
+    res
+      .cookie("accessToken", accessToken, {
+        maxAge: 3600 * 1000,
+        httpOnly: true,
+      })
+      .send();
+    res
+      .cookie("refreshToken", refreshToken, {
+        maxAge: 3600 * 1000,
+        httpOnly: true,
+      })
+      .send();
 
     return res.status(200).json({
-      message: "로그인 성공"
+      message: "로그인 성공",
     });
   } catch (e) {
     if (typeof e === "object" && e?.statusCode === 400) {
       console.log("Error 400:", e);
       return res.status(400).json({
-        message: "아이디 or 비밀번호가 올바르지 않습니다."
+        message: "아이디 or 비밀번호가 올바르지 않습니다.",
       });
     }
     console.log("Error 500:", e);
     return res.status(500).json({
-      message: "서버 내부 에러 발생"
+      message: "서버 내부 에러 발생",
     });
   }
 });
@@ -114,12 +138,12 @@ api.post("/auth/logout", async (req, res) => {
     res.clearCookie("refreshToken").send();
 
     return res.status(200).json({
-      message: "로그아웃 성공"
+      message: "로그아웃 성공",
     });
   } catch (e) {
     console.log("Error 500:", e);
     return res.status(500).json({
-      message: "서버 내부 에러 발생"
+      message: "서버 내부 에러 발생",
     });
   }
 });
@@ -134,7 +158,7 @@ api.delete("/auth/secession", async (req, res) => {
   } catch (e) {
     console.log("Error 500:", e);
     return res.status(500).json({
-      message: "서버 내부 에러 발생"
+      message: "서버 내부 에러 발생",
     });
   }
 });
--- module/generateJwt.js
+++ module/generateJwt.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-  generateJwt: (payload = {}) => {
+  generateJwt: (payload = {}, secretKey) => {
     // jwt 토큰 생성 (accessToken, refreshToken)
     // payload: userId, nickname, profile
 
-    const refreshToken = jwt.sign({}, process.env.JWT_SECRET_KEY, {
+    const refreshToken = jwt.sign({}, secretKey, {
       expiresIn: "14d",
     });
 
-    const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    const accessToken = jwt.sign(payload, secretKey, {
       expiresIn: "2h",
     });
 
